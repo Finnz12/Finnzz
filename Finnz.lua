f07f5f714feebb7b40cbd3a19416cc7422093c10
Finnz12 <155287497+Finnz12@users.noreply.github.com>
2023-12-31 20:08:32 +0700

    Finnz.lua

---
+++ Finnz.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 2753915549 then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Finnz Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "TutorlalConfig"})
+
+
+
+
+
+
+OrionLib:Init()
+end
